2629ec9e7bafe2c5049e4adf6d9f2860ff5d99db
Lance Nines <lance.nines@gmail.com>
2024-05-14 13:54:10 -0400

    game.py added theme

--- game.py
+++ game.py
@@ -1,17 +1,21 @@
 import pygame as pg
+import random as rand
 import dice
 import graphics
 import events
 import logic
+import square
 import animate
 
 pg.init()
 pg.display.set_caption("Dwarf Dice")
 window_icon = pg.image.load("assets/pickaxe.png")
 pg.display.set_icon(window_icon)
-# music = pg.mixer.Sound("audio/rs.mp3")
-# music.play(-1)
-# music.set_volume(.3)
+
+music = pg.mixer.Sound("assets/audio/theme.mp3")
+music.play(-1)
+music.set_volume(.3)
+
 
 clock = pg.time.Clock()
 
@@ -22,16 +26,17 @@ DrawService = graphics.DrawService(WIDTH, HEIGHT)
 EventService = events.EventService()
 LogicService = logic.LogicService()
 AnimateService = animate.AnimateService(DrawService, clock)
+Background = square.Background(WIDTH, HEIGHT)
 
 going = True
 
 playerDice = []
 
-playerDice.append(dice.Die(6, pg.Color(255,255,255)))
-playerDice.append(dice.Die(6, pg.Color(255,0,255)))
-playerDice.append(dice.Die(6, pg.Color(255,255,0)))
-playerDice.append(dice.Die(6, pg.Color(0,255,255)))
-playerDice.append(dice.Die(6, pg.Color(100,100,100)))
+playerDice.append(dice.Die(6, pg.Color(rand.randint(0, 255), rand.randint(0, 255),rand.randint(0, 255))))
+playerDice.append(dice.Die(6, pg.Color(rand.randint(0, 255), rand.randint(0, 255),rand.randint(0, 255))))
+playerDice.append(dice.Die(6, pg.Color(rand.randint(0, 255), rand.randint(0, 255),rand.randint(0, 255))))
+playerDice.append(dice.Die(6, pg.Color(rand.randint(0, 255), rand.randint(0, 255),rand.randint(0, 255))))
+playerDice.append(dice.Die(6, pg.Color(rand.randint(0, 255), rand.randint(0, 255),rand.randint(0, 255))))
 
 total = 0
 for die in playerDice:
@@ -60,11 +65,13 @@ while going:
 
 
     DrawService.resetFrame()
+    Background.runBackground(DrawService.screen)
+    
     diceAndRect = DrawService.drawDice(playerDice) #returns list of (die, rect) for EventService
     LogicService.findHand(playerDice)
-    DrawService.drawValue(LogicService.hand.value)
+    DrawService.drawValue(LogicService.hand.value[0])
                 
     EventService.dieHovered(diceAndRect)
 
-    pg.display.flip()
+    pg.display.update()
     clock.tick(60)
