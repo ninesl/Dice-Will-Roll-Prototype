46f4d646a7853447cff55c313eddf7bf2cb271e6
Lance Nines <lance.nines@gmail.com>
2024-05-20 15:04:18 -0400

    animate hand score

--- animate.py
+++ animate.py
@@ -2,8 +2,52 @@ import random
 import pygame as pg
 
 class AnimateService:
-    def __init__(self, DrawService):
+    def __init__(self, DrawService, clock, fps, SoundService):
         self.DrawService = DrawService
+        self.clock = clock
+        self.SoundService = SoundService
+        self.fps = fps * 10 #fps miliseconds
+
+        self.startingFrame = None
+
+        self.scoreStringIndex = 0
+    
+    def selectedScoreString(self, scoringHandDice):
+        pipChars = "⚀⚁⚂⚃⚄⚅"
+        handVals = ""
+        for die in scoringHandDice:
+            handVals += f"{pipChars[die.curSide.getNum() - 1]}"
+        return handVals.strip()
+    
+    def animateScoringHand(self, LogicService):
+        if LogicService.isScoring:
+            self.scoreString = list(self.scoreString)
+            if not self.startingFrame:
+                self.startingFrame = pg.time.get_ticks()
+
+                self.SoundService.diceDict["tap"].play()
+                self.startingFrame = pg.time.get_ticks()
+            
+                curDieNum = LogicService.scoringHandDice[self.scoreStringIndex].curSide.getNum()
+                self.scoreString[self.scoreStringIndex] = str(curDieNum)
+
+                self.DrawService.deleteRocks(LogicService.scoringHandDice[self.scoreStringIndex].calculate())
+
+                self.scoreStringIndex += 1
+                
+            elif self.startingFrame + self.fps < pg.time.get_ticks():
+                self.startingFrame = None
+                if self.scoreStringIndex == len(self.scoreString):
+                    self.scoreStringIndex = 0
+                    self.startingFrame = None
+                    LogicService.stopScoring()
+
+            self.scoreString = "".join(self.scoreString)
+        else:
+            self.scoreString = self.selectedScoreString(LogicService.scoringHandDice)
+
+        self.DrawService.drawHandInfo(LogicService, self.scoreString)
+        
 
     def shakeDice(self, playerDice, selected = False):
         oldDiceX = self.DrawService.diceX
